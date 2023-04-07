1395a81e0fe5cc2ad4856b3db2a8a693e65972d1
Naveen Dahiya <naveenmicro2002@gmail.com>
2023-04-07 22:17:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose=require("mongoose");
+const authRouter=require("./routes/auth");
+const adminRouter = require("./routes/admin");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const DB="mongodb+srv://adasv9423:Adarsh2001@cluster0.b7smwiy.mongodb.net/?retryWrites=true&w=majority"
+// app.get("/hi",(req,res)=>{
+//     res.send(" jvn bnfj");
+// });
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+mongoose.connect(DB).then(()=>{
+    console.log('connection successful')
+}
+
+).catch(e=>{
+    console.log(e)
+})
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT} `);
+  });
